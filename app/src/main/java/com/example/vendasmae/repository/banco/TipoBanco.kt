20296b19dcbfdc9a0ffcf9320987a0fb441e171d
Kevin-Mattos <kevinmmattos@gmail.com>
2020-11-22 01:17:02 -0300

    Arquivos commit anterior

--- app/src/main/java/com/example/vendasmae/repository/banco/TipoBanco.kt
+++ app/src/main/java/com/example/vendasmae/repository/banco/TipoBanco.kt
@@ -1,4 +1,28 @@
 package com.example.vendasmae.repository.banco
 
-class TipoBanco {
+import com.example.vendasmae.banco.tipos.Tipo
+import com.example.vendasmae.banco.tipos.TipoDao
+import com.example.vendasmae.banco.vendas.Venda
+
+class TipoBanco  (val tipoDao: TipoDao) {
+
+    fun getAll() = tipoDao.getAll()
+
+    fun insert(vendedora: Tipo){
+        val executa = { tipoDao.insertLista(vendedora)}
+        BaseAsyncTask(executa).execute()
+    }
+
+    fun removeAll() {
+        val executa = { tipoDao.removeAll()}
+        BaseAsyncTask(executa).execute()
+    }
+
+    fun insertMultlple(vendas: List<Tipo>) {
+        val executa = { tipoDao.insertMultiple(vendas)}
+        BaseAsyncTask(executa).execute()
+    }
+
+    //fun getVendaEVendedora() = tipoDao.getVendaAndVendedora()
+
 }
